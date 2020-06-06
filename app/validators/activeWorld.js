4848cccb8afe2fd6001473c27a2eeed8572a387b
Matt Cavanagh <maelstrome26@gmail.com>
2020-06-06 16:06:06 +0100

    Added flag to prevent logging all validation rejections and generating shittons of log spam.

--- app/validators/activeWorld.js
+++ app/validators/activeWorld.js
@@ -4,7 +4,9 @@ function validate (payload) {
     if (payload.hasOwnProperty("world_id")) {
         const world_id = parseInt(payload.world_id);
         if (!flags.MONITORED_SERVERS.includes(world_id)) {
-            console.log(`Got event from world ${payload.world_id}, which we don't monitor!`);
+            if (flags.LOGGING.VALIDATION_REJECTS) {
+                console.log(`Got event from world ${payload.world_id}, which we don't monitor!`);
+            }
             return false;
         }
 
