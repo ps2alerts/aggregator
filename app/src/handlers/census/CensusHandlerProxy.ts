b9765bf75caacfd54446cfc0bb62b8be878efc03
Matt Cavanagh <maelstrome26@gmail.com>
2020-06-30 21:42:39 +0100

    Made a start on the handler proxy

---
+++ app/src/handlers/census/CensusHandlerProxy.ts
@@ -0,0 +1,68 @@
+import WorldValidator from '../../validators/WorldValidator';
+import { GenericEvent } from 'ps2census/dist/client/utils/PS2Events';
+
+export default class CensusHandlerProxy {
+    public constructor(
+        private worldCheck:WorldValidator
+    ) {
+
+    }
+
+    public async handle(payload:GenericEvent): boolean {
+        // Validate if the message is relevent for what we want, e.g. worlds and zones with active alerts on.
+        if (!this.worldCheck.validate(parseInt(payload.world_id))) {
+            return false;
+        }
+
+        switch (payload.event_name) {
+            case 'AchievementEarned':
+                eventStore.storeAchievementEarned(payload);
+                break;
+            case 'BattleRankUp':
+                eventStore.storeBattleRankUp(payload);
+                break;
+            case 'Death':
+                eventStore.storeDeath(payload);
+                break;
+            case 'FacilityControl':
+                eventStore.storeFacilityControl(payload);
+                break;
+            case 'GainExperience':
+                eventStore.storeGainExperience(payload);
+                break;
+            case 'ItemAdded':
+                eventStore.storeItemAdded(payload);
+                break;
+            case 'MetagameEvent':
+                eventStore.storeMetagameEvent(payload);
+                break;
+            case 'PlayerFacilityCapture':
+                eventStore.storePlayerFacilityCapture(payload);
+                break;
+            case 'PlayerFacilityDefend':
+                eventStore.storePlayerFacilityDefend(payload);
+                break;
+            case 'PlayerLogin':
+                eventStore.storeLogin(payload.character_id,payload.event_name,payload.timestamp,payload.world_id);
+                break;
+            case 'PlayerLogout':
+                eventStore.storeLogout(payload.character_id,payload.event_name,payload.timestamp,payload.world_id);
+                break;
+            case 'SkillAdded':
+                eventStore.storeSkillAdded(payload);
+                break;
+            case 'VehicleDestroy':
+                eventStore.storeVehicleDestroy(payload);
+                break;
+            case 'ContinentLock':
+                eventStore.storeContinentLock(payload);
+                break;
+            case 'ContinentUnlock':
+                eventStore.storeContinentUnlock(payload);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+}
