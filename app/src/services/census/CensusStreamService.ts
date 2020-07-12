0651b0b21f6242de039162431fa35ee9f8b2fdb9
marci4 <admin@marci4.de>
2020-07-12 17:55:57 +0200

    Update ps2census to version 0.2.6

--- app/src/services/census/CensusStreamService.ts
+++ app/src/services/census/CensusStreamService.ts
@@ -1,20 +1,20 @@
 // @See https://github.com/microwavekonijn/ps2census for further commands (wsClient)
 
 import ServiceInterface from '../../interfaces/ServiceInterface';
-import PS2EventClient from 'ps2census/dist/client/Client'; // TODO: Await microwave's type fixes
 import {getLogger} from '../../logger';
 import {injectable} from 'inversify';
 import CensusProxy from '../../handlers/census/CensusProxy';
+import {Client} from 'ps2census';
 
 @injectable()
 export default class CensusStreamService implements ServiceInterface {
     private static readonly logger = getLogger('ps2census');
 
-    private readonly wsClient: PS2EventClient;
+    private readonly wsClient: Client;
     private readonly censusProxy: CensusProxy;
     private readonly subscriptions = [];
 
-    constructor(wsClient: PS2EventClient, censusProxy: CensusProxy) {
+    constructor(wsClient: Client, censusProxy: CensusProxy) {
         this.wsClient = wsClient;
         this.censusProxy = censusProxy;
     }
