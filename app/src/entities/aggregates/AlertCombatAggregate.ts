05774177dadf90571f6ad1b1a8c396e84dab0f2b
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-15 02:20:54 +0100

    Added AlertCombatAggregate

---
+++ app/src/entities/aggregates/AlertCombatAggregate.ts
@@ -0,0 +1,57 @@
+import {Entity, Column} from 'typeorm';
+
+@Entity()
+export class AlertCombatAggregate {
+
+    @Column
+    alertId: number;
+
+    @Column('simple-json')
+    kills: {
+        vs: number;
+        nc: number;
+        tr: number;
+        nso: number;
+    };
+
+    @Column('simple-json')
+    deaths: {
+        vs: number;
+        nc: number;
+        tr: number;
+        nso: number;
+    };
+
+    @Column('simple-json')
+    teamKills: {
+        vs: number;
+        nc: number;
+        tr: number;
+        nso: number;
+    };
+
+    @Column('simple-json')
+    suicides: {
+        vs: number;
+        nc: number;
+        tr: number;
+        nso: number;
+    };
+
+    @Column('simple-json')
+    headshots: {
+        vs: number;
+        nc: number;
+        tr: number;
+        nso: number;
+    };
+
+    @Column('simple-json')
+    totals: {
+        kills: number;
+        deaths: number;
+        teamKills: number;
+        suicides: number;
+        headshots: number;
+    };
+}
