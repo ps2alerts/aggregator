303848c2363204464ac2390d48c2420cef5c9838
Microwavekonijn <sjorsw@protonmail.ch>
2020-07-15 17:56:22 +0200

    The International olympic Committee approves now

--- app/src/services/mongo/index.ts
+++ app/src/services/mongo/index.ts
@@ -1,15 +1,21 @@
-import {ContainerModule} from 'inversify';
-import ServiceInterface, {SERVICE} from '../../interfaces/ServiceInterface';
+import { ContainerModule } from 'inversify';
+import ServiceInterface, { SERVICE } from '../../interfaces/ServiceInterface';
 import MongoDatabaseConnectionService from './MongoDatabaseConnectionService';
 import MongoDBConnection from './MongoDBConnection';
 import config from '../../config';
+import { Mongoose } from 'mongoose';
+import Database from '../../config/database';
 
 export default new ContainerModule((bind) => {
     bind<ServiceInterface>(SERVICE).to(MongoDatabaseConnectionService);
 
+    bind<Mongoose>(Mongoose)
+        .toDynamicValue(() => new Mongoose())
+        .inSingletonScope();
+
+    bind<Database>('mongooseConfig').toConstantValue(config.database);
+
     bind(MongoDBConnection)
-        .toDynamicValue(() => new MongoDBConnection(
-            config.database,
-        ))
+        .toSelf()
         .inSingletonScope();
 });
