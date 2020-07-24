e9248150a3432f657db62748a95ee27f07f7d29a
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-24 01:12:49 +0100

    Think it ran plan without the rest :-?

--- provisioning/terraform/production.tf
+++
@@ -1,8 +0,0 @@
-module "websocket_production" {
-  source           = "./modules/websocket"
-  namespace        = "ps2alerts"
-  environment      = "production"
-  identifier       = "ps2alerts-websocket-production"
-  url              = "wss.ps2alerts.com"
-  checksum_version = var.checksum_version
-}
