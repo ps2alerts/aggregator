9d807ac779e00c2b3fb471425f7ab7d28ff0199d
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-25 01:39:08 +0100

    Apparently only text versions are not allowed

--- app/src/config/logger.ts
+++ app/src/config/logger.ts
@@ -1,6 +1,5 @@
 import {get} from '../utils/env';
 
 export default class Logger {
-    // eslint-disable-next-line yoda
     public readonly level = get('LOG_LEVEL', get('NODE_ENV', 'development') === 'development' ? 'debug' : 'info');
 }
