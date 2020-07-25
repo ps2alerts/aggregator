cfa6d5f3e4fa259a281e2e48e3706f56dd965551
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-25 02:19:10 +0100

    Satisfy linter

--- app/src/services/census/CensusStreamService.ts
+++ app/src/services/census/CensusStreamService.ts
@@ -3,10 +3,7 @@
 import ServiceInterface from '../../interfaces/ServiceInterface';
 import {getLogger} from '../../logger';
 import {injectable} from 'inversify';
-import {Client, MetagameEvent, PS2Event, Events} from 'ps2census';
-import {getUnixTimestamp} from '../../utils/time';
-import {World} from '../../constants/world';
-import {MetagameEventIds} from '../../constants/metagameEventIds';
+import {Client, PS2Event} from 'ps2census';
 
 @injectable()
 export default class CensusStreamService implements ServiceInterface {
@@ -70,24 +67,6 @@ export default class CensusStreamService implements ServiceInterface {
 
         this.wsClient.on('subscribed', () => {
             CensusStreamService.logger.info('Census stream subscribed!');
-
-            // Uncomment if you wish to inject an alert and monitor
-            // const event = new MetagameEvent(this.wsClient, {
-            //     event_name: 'MetagameEvent',
-            //     experience_bonus: '25.000000',
-            //     faction_nc: '6.274510',
-            //     faction_tr: '19.607843',
-            //     faction_vs: '9.803922',
-            //     instance_id: String(Math.floor(Math.random() * 100000) + 1),
-            //     metagame_event_id: String(MetagameEventIds.MELTDOWN_AMERISH),
-            //     metagame_event_state: '137',
-            //     metagame_event_state_name: 'started',
-            //     timestamp: String(getUnixTimestamp()),
-            //     world_id: String(World.MILLER),
-            //
-            // });
-            // this.wsClient.emit(Events.PS2_META_EVENT, event);
-            // CensusStreamService.logger.debug('Emitted Metagame Start event');
         });
     }
 }
