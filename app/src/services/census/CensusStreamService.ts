014c5303aa82c148786ee7dc4c91772b5479f1ca
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-29 00:08:50 +0100

    Updated comment

--- app/src/services/census/CensusStreamService.ts
+++ app/src/services/census/CensusStreamService.ts
@@ -136,7 +136,7 @@ export default class CensusStreamService implements ServiceInterface {
 
             this.lastMessagesMap.forEach((lastTime: number, world: World) => {
                 const thresholdLimit = 120000;
-                const threshold: number = Date.now() - thresholdLimit; // We expect to get at least one death event on every world, regarless of time within 60 seconds
+                const threshold: number = Date.now() - thresholdLimit; // We expect to get at least one death event on every world, regardless of time within 120 seconds
 
                 if (lastTime < threshold) {
                     CensusStreamService.logger.error(`No Census Death messages received on world ${world} within expected threshold of ${thresholdLimit / 1000} seconds. Assuming dead subscription. Rebooting Connection.`);
