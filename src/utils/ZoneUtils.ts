efe1baaa489fa9a42a14900035fb2cdeccb5a5df
Matt <maelstrome26@gmail.com>
2020-08-01 04:00:09 +0100

    Further zone tweaks

--- src/utils/ZoneUtils.ts
+++ src/utils/ZoneUtils.ts
@@ -12,12 +12,6 @@ export default class ZoneUtils {
                 return Zone.AMERISH;
             case Zone.ESAMIR:
                 return Zone.ESAMIR;
-            case Zone.VR_TRAINING_NC:
-                return Zone.VR_TRAINING_NC;
-            case Zone.VR_TRAINING_TR:
-                return Zone.VR_TRAINING_TR;
-            case Zone.VR_TRAINING_VS:
-                return Zone.VR_TRAINING_VS;
             default:
                 throw new IllegalArgumentException(`Unknown zone: ${value}`, 'ZoneUtils.parse');
         }
