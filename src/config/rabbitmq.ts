5516888db9575c7dfa5ea1efbe063a9d9d5b4152
Matt Cavanagh <maelstrome26@gmail.com>
2020-08-20 02:27:52 +0100

    Updated exchange name default

--- src/config/rabbitmq.ts
+++ src/config/rabbitmq.ts
@@ -9,7 +9,7 @@ export default class RabbitMQ {
     public readonly port = getInt('RABBITMQ_PORT', 5672);
     public readonly user = get('RABBITMQ_USER', 'user');
     public readonly pass = get('RABBITMQ_PASS', 'bitnami');
-    public readonly exchange = get('RABBITMQ_EXCHANGE', 'ps2alertsExchange');
+    public readonly exchange = get('RABBITMQ_EXCHANGE', 'ps2alerts');
     public readonly vhost = get('RABBITMQ_VHOST', 'ps2alerts');
     public readonly heartbeat = 10;
     public readonly timeout = 10000;
