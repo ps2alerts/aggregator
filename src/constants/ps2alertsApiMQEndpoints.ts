7e2c362ccfce7aff46bc4112e6ef1a60d0f3d153
Matt Cavanagh <maelstrome26@gmail.com>
2020-09-25 00:47:07 +0100

    Added all expected endpoint names

--- src/constants/ps2alertsApiMQEndpoints.ts
+++ src/constants/ps2alertsApiMQEndpoints.ts
@@ -1,5 +1,20 @@
+// CAREFUL modifying this file! The values have to match what the API expects exactly or we will get
+// unprocessable messages!
 export enum Ps2alertsApiMQEndpoints {
-    INSTANCE_DEATH = 'instanceDeath',
-    INSTANCE_FACILITY_CONTROL = 'instanceFacilityControl',
     GLOBAL_CHARACTER_AGGREGATE = 'globalCharacterAggregate',
+    GLOBAL_CLASS_AGGREGATE = 'globalClassAggregate',
+    GLOBAL_FACILITY_CONTROL_AGGREGATE = 'globalFacilityControlAggregate',
+    GLOBAL_FACTION_COMBAT_AGGREGATE = 'globalFactionCombatAggregate',
+    GLOBAL_OUTFIT_AGGREGATE = 'globalOutfitAggregate',
+    GLOBAL_WEAPON_AGGREGATE = 'globalWeaponAggregate',
+    INSTANCE_CHARACTER_AGGREGATE = 'instanceCharacterAggregate',
+    INSTANCE_CLASS_AGGREGATE = 'instanceClassAggregate',
+    INSTANCE_FACILITY_CONTROL_AGGREGATE = 'instanceFacilityControlAggregate',
+    INSTANCE_FACTION_COMBAT_AGGREGATE = 'instanceFactionCombatAggregate',
+    INSTANCE_OUTFIT_AGGREGATE = 'instanceOutfitAggregate',
+    INSTANCE_POPULATION_AGGREGATE = 'instancePopulationAggregate',
+    INSTANCE_WEAPON_AGGREGATE = 'instanceWeaponAggregate',
+    INSTANCE_DEATH = 'instanceDeathEvent',
+    INSTANCE_FACILITY_CONTROL = 'instanceFacilityControlEvent',
+    INSTANCE_METAGAME = 'instanceMetagame',
 }
