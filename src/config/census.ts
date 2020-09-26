8972af98080fcf5ccb771da624483d84ad62af3d
Matt Cavanagh <maelstrome26@gmail.com>
2020-09-27 00:11:47 +0100

    Disable high traffic events

--- src/config/census.ts
+++ src/config/census.ts
@@ -11,7 +11,8 @@ export default class Census {
         environment: 'ps2',
         streamManagerConfig: {
             subscription: {
-                eventNames: ['Death', 'FacilityControl', 'MetagameEvent', 'PlayerLogin', 'PlayerLogout', 'GainExperience'],
+                // eventNames: ['Death', 'FacilityControl', 'MetagameEvent', 'PlayerLogin', 'PlayerLogout', 'GainExperience'],
+                eventNames: ['FacilityControl', 'MetagameEvent', 'PlayerLogin', 'PlayerLogout'],
                 worlds: ['all'],
                 characters: ['all'],
                 logicalAndCharactersWithWorlds: true,
