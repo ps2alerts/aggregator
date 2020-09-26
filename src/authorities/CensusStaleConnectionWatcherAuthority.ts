0cd5670d0505a181b385027c808c17646d3e257e
Matt Cavanagh <maelstrome26@gmail.com>
2020-09-26 22:25:54 +0100

    Memory leak fix

--- src/authorities/CensusStaleConnectionWatcherAuthority.ts
+++ src/authorities/CensusStaleConnectionWatcherAuthority.ts
@@ -74,5 +74,8 @@ export default class CensusStaleConnectionWatcherAuthority {
                 void this.wsClient.resubscribe();
             }
         });
+
+        // Empty the map so we don't memory leak the living daylights out of the app
+        map.clear();
     }
 }
