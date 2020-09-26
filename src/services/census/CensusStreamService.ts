91511cceb994838beeb07a6c577c01437c074271
Matt Cavanagh <maelstrome26@gmail.com>
2020-09-26 13:39:24 +0100

    Downgraded Census debug messages to silly, cos there's a *lot*

--- src/services/census/CensusStreamService.ts
+++ src/services/census/CensusStreamService.ts
@@ -108,7 +108,7 @@ export default class CensusStreamService implements ServiceInterface {
                 !message.includes('Heartbeat acknowledged') &&
                 !message.includes('CharacterManager')
             ) {
-                CensusStreamService.logger.debug(`Census stream debug: ${message}`);
+                CensusStreamService.logger.silly(`Census stream debug: ${message}`);
             }
         });
 
