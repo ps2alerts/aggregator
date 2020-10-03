3d8ac1900b2026f44990b16c7e99d1823ffe6b3b
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-03 16:26:53 +0100

    Friggin' IDE keeps deleting brackets which are needed :sob:

--- provisioning/terraform/modules/aggregator/datadog_monitors.tf
+++ provisioning/terraform/modules/aggregator/datadog_monitors.tf
@@ -1,7 +1,7 @@
 resource datadog_monitor "aggregator_not_running" {
   name = "PS2Alerts Aggregator not running [${var.environment}]"
   type = "metric alert"
-  query = "sum(last_1m):avg:kubernetes.pods.running{kube_deployment:ps2alerts-aggregator-${var.environment} <= 0"
+  query = "sum(last_1m):avg:kubernetes.pods.running{kube_deployment:ps2alerts-aggregator-${var.environment}} <= 0"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator not running"})
 
   thresholds = {
@@ -18,7 +18,7 @@ resource datadog_monitor "aggregator_not_running" {
 resource datadog_monitor "aggregator_high_mem" {
   name = "PS2Alerts Aggregator high memory [${var.environment}]"
   type = "metric alert"
-  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-aggregator-${var.environment} > 262144000"
+  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-aggregator-${var.environment}} > 262144000"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high memory"})
 
   thresholds = {
@@ -36,7 +36,7 @@ resource datadog_monitor "aggregator_high_mem" {
 resource datadog_monitor "aggregator_high_cpu" {
   name = "PS2Alerts Aggregator high CPU [${var.environment}]"
   type = "metric alert"
-  query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-aggregator-${var.environment} > 450000000"
+  query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-aggregator-${var.environment}} > 450000000"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high CPU"})
 
   thresholds = {
@@ -72,7 +72,7 @@ resource datadog_monitor "aggregator_high_errors" {
 resource datadog_monitor "aggregator_high_restarts_long" {
   name = "PS2Alerts Aggregator high restarts (long) [${var.environment}]"
   type = "query alert"
-  query = "avg(last_1d):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}, 'agile', 2, direction='above', alert_window='last_1h', interval=300, count_default_zero='true', seasonality='hourly') >= 1"
+  query = "avg(last_1d):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}}, 'agile', 2, direction='above', alert_window='last_1h', interval=300, count_default_zero='true', seasonality='hourly') >= 1"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high restarts (long)"})
 
   thresholds = {
@@ -89,7 +89,7 @@ resource datadog_monitor "aggregator_high_restarts_long" {
 resource datadog_monitor "aggregator_high_restarts_short" {
   name = "PS2Alerts Aggregator high restarts (short) [${var.environment}]"
   type = "query alert"
-  query = "avg(last_1h):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}, 'agile', 2, direction='above', alert_window='last_5m', interval=20, count_default_zero='true', seasonality='hourly') >= 1"
+  query = "avg(last_1h):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}}, 'agile', 2, direction='above', alert_window='last_5m', interval=20, count_default_zero='true', seasonality='hourly') >= 1"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high restarts (long)"})
 
   thresholds = {
