9fbcfef6dbb6452318615130fe865b6d418787a6
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-03 16:06:50 +0100

    I need more tea...

--- provisioning/terraform/modules/aggregator/datadog_monitors.tf
+++ provisioning/terraform/modules/aggregator/datadog_monitors.tf
@@ -2,7 +2,7 @@ resource datadog_monitor "aggregator_not_running" {
   name = "PS2Alerts Aggregator not running [${var.environment}]"
   type = "metric alert"
   query = "sum(last_1m):avg:kubernetes.pods.running{kube_deployment:ps2alerts-aggregator-${var.environment} <= 0"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator not running"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator not running"})
 
   thresholds = {
     critical = 0
@@ -19,7 +19,7 @@ resource datadog_monitor "aggregator_high_mem" {
   name = "PS2Alerts Aggregator high memory [${var.environment}]"
   type = "metric alert"
   query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-aggregator-${var.environment} > 262144000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator high memory"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high memory"})
 
   thresholds = {
     critical = 262144000
@@ -37,7 +37,7 @@ resource datadog_monitor "aggregator_high_cpu" {
   name = "PS2Alerts Aggregator high CPU [${var.environment}]"
   type = "metric alert"
   query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-aggregator-${var.environment} > 450000000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator high CPU"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high CPU"})
 
   thresholds = {
     critical = 450000000
@@ -55,7 +55,7 @@ resource datadog_monitor "aggregator_high_errors" {
   name = "PS2Alerts Aggregator high errors [${var.environment}]"
   type = "log alert"
   query = "logs(\"container_name:*aggregator\\-${var.environment}* status:error\").index(\"*\").rollup(\"count\").last(\"10m\") > 200"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator high errors"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high errors"})
 
   thresholds = {
     critical = 200
@@ -73,7 +73,7 @@ resource datadog_monitor "aggregator_high_restarts_long" {
   name = "PS2Alerts Aggregator high restarts (long) [${var.environment}]"
   type = "query alert"
   query = "avg(last_1d):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}, 'agile', 2, direction='above', alert_window='last_1h', interval=300, count_default_zero='true', seasonality='hourly') >= 1"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator high restarts (long)"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high restarts (long)"})
 
   thresholds = {
     critical = 1
@@ -90,7 +90,7 @@ resource datadog_monitor "aggregator_high_restarts_short" {
   name = "PS2Alerts Aggregator high restarts (short) [${var.environment}]"
   type = "query alert"
   query = "avg(last_1h):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}, 'agile', 2, direction='above', alert_window='last_5m', interval=20, count_default_zero='true', seasonality='hourly') >= 1"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, aggregator: "Aggregator", description: "Aggregator high restarts (long)"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "Aggregator high restarts (long)"})
 
   thresholds = {
     critical = 1
