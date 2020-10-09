80d0fd3e78c07f029dd325b2e5b67ac2c5466419
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-09 03:18:15 +0100

    Dialed in aggregator RAM monitor

--- provisioning/terraform/modules/aggregator/datadog_monitors.tf
+++ provisioning/terraform/modules/aggregator/datadog_monitors.tf
@@ -18,12 +18,11 @@ resource datadog_monitor "aggregator_not_running" {
 resource datadog_monitor "aggregator_high_mem" {
   name = "PS2Alerts Aggregator high memory [${var.environment}]"
   type = "metric alert"
-  query = "avg(last_10m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-aggregator-${var.environment}} > 471859000"
+  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-aggregator-${var.environment}} > 471859000"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "high memory"})
 
   thresholds = {
     critical = 471859000 #450MB
-    warning = 419430000 #400MB
   }
 
   notify_no_data = true
@@ -41,7 +40,6 @@ resource datadog_monitor "aggregator_high_cpu" {
 
   thresholds = {
     critical = 150000000 # 0.150 CPU
-    warning = 100000000 # 0.1 CPU
   }
 
   notify_no_data = true
