826eb2f07f940f577a040f342aa38aabdd91b39a
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-12 01:09:54 +0100

    Removed vehicle destroy raw event from API messages, not supported

--- src/constants/ps2alertsApiMQEndpoints.ts
+++ src/constants/ps2alertsApiMQEndpoints.ts
@@ -22,5 +22,4 @@ export enum Ps2alertsApiMQEndpoints {
     INSTANCE_DEATH = 'instanceDeathEvent',
     INSTANCE_FACILITY_CONTROL = 'instanceFacilityControlEvent',
     INSTANCE_METAGAME = 'instanceMetagame',
-    INSTANCE_VEHICLE_DESTROY = 'instanceVehicleDestroyEvent'
 }
