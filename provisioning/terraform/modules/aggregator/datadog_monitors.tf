7e37fed085463f3c1eda1d9c67671c7177bfc4b7
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-18 20:37:05 +0100

    Increased CPU monitor

--- provisioning/terraform/modules/aggregator/datadog_monitors.tf
+++ provisioning/terraform/modules/aggregator/datadog_monitors.tf
@@ -35,11 +35,11 @@ resource datadog_monitor "aggregator_high_mem" {
 resource datadog_monitor "aggregator_high_cpu" {
   name = "PS2Alerts Aggregator high CPU [${var.environment}]"
   type = "metric alert"
-  query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-aggregator-${var.environment}} > 150000000"
+  query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-aggregator-${var.environment}} > 175000000"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "high CPU"})
 
   thresholds = {
-    critical = 150000000 # 0.150 CPU
+    critical = 175000000 # 0.175 CPU
   }
 
   notify_no_data = true
