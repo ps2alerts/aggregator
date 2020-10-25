487891c31a225d8bb0266ac35ded51b009f2d4b4
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-25 15:54:14 +0000

    Underped query

--- provisioning/terraform/modules/aggregator/datadog_monitors.tf
+++ provisioning/terraform/modules/aggregator/datadog_monitors.tf
@@ -69,7 +69,7 @@ resource datadog_monitor "aggregator_high_errors" {
 resource datadog_monitor "aggregator_high_restarts" {
   name = "PS2Alerts Aggregator restarts [${var.environment}]"
   type = "query alert"
-  query = "change(sum(last_5m),last_5m):avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment} > 0.5}, 'agile', 2, direction='above', alert_window='last_5m', interval=20, count_default_zero='true', seasonality='hourly') >= 1"
+  query = "change(sum(last_5m),last_5m):avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-aggregator-${var.environment}} > 0.5"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "Aggregator", description: "restarts"})
 
   thresholds = {
