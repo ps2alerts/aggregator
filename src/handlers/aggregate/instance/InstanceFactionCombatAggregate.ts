65a37c68b3ad1d820265a9598b59cba6a3bf0863
Matt Cavanagh <maelstrome26@gmail.com>
2020-12-12 19:40:46 +0000

    Fixed NSO kills and deaths not showing up #199

--- src/handlers/aggregate/instance/InstanceFactionCombatAggregate.ts
+++ src/handlers/aggregate/instance/InstanceFactionCombatAggregate.ts
@@ -23,14 +23,34 @@ export default class InstanceFactionCombatAggregate implements AggregateHandlerI
 
         const documents = [];
 
-        // Increment attacker faction kills
-        if (event.killType === Kill.Normal && event.attackerCharacter) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
-            const attackerKillKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.kills`;
-            documents.push(
-                {$inc: {[attackerKillKey]: 1}},
-                {$inc: {['totals.kills']: 1}},
-            );
+        if (event.attackerCharacter) {
+            // Increment attacker faction kills
+            if (event.killType === Kill.Normal || event.killType === Kill.Undetermined) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
+                const attackerKillKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.kills`;
+                documents.push(
+                    {$inc: {[attackerKillKey]: 1}},
+                    {$inc: {['totals.kills']: 1}},
+                );
+            }
+
+            if (event.killType === Kill.TeamKill) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
+                const teamKillKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.teamKills`;
+                documents.push(
+                    {$inc: {[teamKillKey]: 1}},
+                    {$inc: {['totals.teamKills']: 1}},
+                );
+            }
+
+            if (event.isHeadshot) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
+                const attackerHeadshotKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.headshots`;
+                documents.push(
+                    {$inc: {[attackerHeadshotKey]: 1}},
+                    {$inc: {['totals.headshots']: 1}},
+                );
+            }
         }
 
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
@@ -40,30 +60,12 @@ export default class InstanceFactionCombatAggregate implements AggregateHandlerI
             {$inc: {['totals.deaths']: 1}},
         );
 
-        if (event.killType === Kill.TeamKill && event.attackerCharacter) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
-            const teamKillKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.teamKills`;
-            documents.push(
-                {$inc: {[teamKillKey]: 1}},
-                {$inc: {['totals.teamKills']: 1}},
-            );
-        }
-
         if (event.killType === Kill.Suicide || event.killType === Kill.RestrictedArea) {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
             const suicideKey = `${FactionUtils.parseFactionIdToShortName(event.character.faction)}.suicides`;
             documents.push(
                 {$inc: {[suicideKey]: 1}},
                 {$inc: {['totals.suicides']: 1}},
-            );
-        }
-
-        if (event.isHeadshot && event.attackerCharacter) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/restrict-template-expressions
-            const attackerHeadshotKey = `${FactionUtils.parseFactionIdToShortName(event.attackerCharacter.faction)}.headshots`;
-            documents.push(
-                {$inc: {[attackerHeadshotKey]: 1}},
-                {$inc: {['totals.headshots']: 1}},
             );
         }
 
