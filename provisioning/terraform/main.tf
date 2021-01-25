627385fe5b980ad3c994ce7e2fe4cf48bce69de9
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-25 19:07:52 +0000

    Removed depricated provider line

--- provisioning/terraform/main.tf
+++ provisioning/terraform/main.tf
@@ -11,9 +11,8 @@ data "digitalocean_kubernetes_cluster" "cluster" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
-  host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
-  token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+  host  = data.digitalocean_kubernetes_cluster.cluster.endpoint
+  token = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
   )
