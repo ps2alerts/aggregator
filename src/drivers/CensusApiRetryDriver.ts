4f26cd5eac29fc6bb4d7552c6265fccbc0845e2a
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-26 22:38:26 +0000

    Extended timeout and attempt numbers for CensusApiRetryDriver. Added logging when it succeeded

--- src/drivers/CensusApiRetryDriver.ts
+++ src/drivers/CensusApiRetryDriver.ts
@@ -4,7 +4,8 @@ import {getLogger} from '../logger';
 export class CensusApiRetryDriver<T> {
     private static readonly logger = getLogger('CensusApiRetryDriver');
 
-    private readonly retryLimit = 3;
+    private readonly retryLimit = 6;
+    private readonly delayTime = 5000; // 30 seconds total for waiting for the API to recover
     private readonly getMethod: Promise<T>;
     private readonly caller: string;
 
@@ -18,10 +19,16 @@ export class CensusApiRetryDriver<T> {
 
         try {
             CensusApiRetryDriver.logger.debug(`[${this.caller}] Attempting to get data from Census... Attempt #${attempts}`);
-            return await this.getMethod;
+            const res = await this.getMethod;
+
+            if (attempts > 1) {
+                CensusApiRetryDriver.logger.info(`[${this.caller}] Retry for Census successful at attempt #${attempts}`);
+            }
+
+            return res;
         } catch (err) {
             if (attempts < this.retryLimit) {
-                await this.delay(2500);
+                await this.delay(this.delayTime);
 
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
                 CensusApiRetryDriver.logger.warn(`[${this.caller}] Census Request failed! Retrying... Attempt #${attempts}. E: ${err.message}`);
