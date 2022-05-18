65545333a0b6e9ddd9b648a2c0ff66b163b7673b
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-18 18:08:11 +0100

    Reinstated memerald

--- src/services/census/CensusStream.ts
+++ src/services/census/CensusStream.ts
@@ -73,7 +73,7 @@ export default class CensusStream {
         this.censusClient.on('ps2Event', (event: PS2Event) => {
             const worldId = parseInt(event.world_id, 10);
 
-            if ([World.JAEGER, World.EMERALD].includes(worldId)) {
+            if ([World.JAEGER].includes(worldId)) {
                 return true;
             }
 
