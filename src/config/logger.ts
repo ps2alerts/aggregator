274b78251d2dae1376a836d6bd7d464513387a6c
Matt Cavanagh <maelstrome26@gmail.com>
2022-07-03 08:27:01 +0100

    Force debug in prd

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -18,7 +18,8 @@ export interface TransportConfig<K extends keyof TransportsIndex = keyof Transpo
 
 export default class Logger {
     public readonly levels = config.npm.levels;
-    public readonly level = get('LOG_LEVEL', get('NODE_ENV', 'development') === 'development' ? 'debug' : 'info');
+    // public readonly level = get('LOG_LEVEL', get('NODE_ENV', 'development') === 'development' ? 'debug' : 'info');
+    public readonly level = 'debug';
     public readonly globalFilter: LogFilter = [];
     public readonly transport: string | string[] = get('LOGGER_TRANSPORTS', 'consoleDev').split(',');
     public readonly transports: Record<string, TransportConfig> = {
