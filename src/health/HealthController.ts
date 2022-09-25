e0177040abcd3a7361889c702394aafc7febb81e
Microwavekonijn <microwavekonijn@pm.me>
2022-09-26 00:25:13 +0200

    feat: added dependent services to health check

--- src/health/HealthController.ts
+++ src/health/HealthController.ts
@@ -1,17 +1,46 @@
 import {Controller, Get} from '@nestjs/common';
-import {HealthCheck, HealthCheckResult, HealthCheckService} from '@nestjs/terminus';
+import {
+    HealthCheck,
+    HealthCheckResult,
+    HealthCheckService,
+    HttpHealthIndicator,
+    MicroserviceHealthIndicator
+} from '@nestjs/terminus';
+import config from '../config';
+import {MicroserviceOptions, RedisOptions, Transport} from '@nestjs/microservices';
 
 @Controller('health')
 export class HealthController {
     constructor(
         private readonly health: HealthCheckService,
+        private readonly http: HttpHealthIndicator,
+        private readonly microservice: MicroserviceHealthIndicator,
     ) {
     }
 
     @Get()
     @HealthCheck()
     public check(): Promise<HealthCheckResult> {
-        // TODO: Currently empty but can be used to check health or Redis and RabbitMQ
-        return this.health.check([]);
+        return this.health.check([
+            () => this.http.pingCheck('api', config.internalApi.host, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                auth: {
+                    username: config.internalApi.username,
+                    password: config.internalApi.password,
+                },
+            }),
+            () => this.microservice.pingCheck<RedisOptions>('redis', {
+                transport: Transport.REDIS,
+                options: config.redis,
+            }),
+            () => this.microservice.pingCheck<MicroserviceOptions>('rabbitmq', {
+                transport: Transport.RMQ,
+                options: {
+                    urls: config.rabbitmq.connectionUrl,
+                },
+            }),
+        ]);
     }
 }
