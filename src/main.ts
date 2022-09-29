c191992bb3cef7e6d560719ee6ecc6b753088d09
Microwavekonijn <microwavekonijn@pm.me>
2022-09-29 15:38:53 +0200

    feat(error): added error handler

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import {NestFactory} from '@nestjs/core';
 import AppModule from './AppModule';
 import config from './config';
 import {FastifyAdapter} from '@nestjs/platform-fastify';
+import {Logger} from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
     process.on('unhandledRejection', (err) => {
         throw err;
+    }).on('uncaughtException', (err) => {
+        const logger = new Logger('Exception');
+
+        logger.error(err);
+
+        process.exit(1);
     });
 
     const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
