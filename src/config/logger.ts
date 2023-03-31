0d22a1ff617fbf4c50cc3df13075fb21fbd4afd9
Matt Cavanagh <maelstrome26@gmail.com>
2023-03-31 09:52:03 +0200

    Added debug to default log levels

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,5 +3,5 @@ import {get, getBool} from '../utils/env';
 
 export default class Logger {
     public readonly silly = getBool('LOG_SILLY', false);
-    public readonly levels = get('LOG_LEVELS', 'error,warn,log').split(',') as LogLevel[];
+    public readonly levels = get('LOG_LEVELS', 'error,warn,log,debug').split(',') as LogLevel[];
 }
